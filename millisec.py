38f60b0f7402e3978565a57d4990da8209b659e4
mohsen shabanian <mohsen1299@gmail.com>
2019-05-05 20:24:00 +0430

    update mil

--- millisec.py
+++ millisec.py
@@ -1,11 +1,11 @@
 def mytime(mil):
     mil = int(mil)
-    seconds=(millis/1000)%60
+    seconds=(mil/1000)%60
     seconds = int(seconds)
-    minutes=(millis/(1000*60))%60
+    minutes=(mil/(1000*60))%60
     minutes = int(minutes)
-    hours=(millis/(1000*60*60))%24
-    print (hours+":"+minutes+":"+seconds)
+    hours=int(mil/(1000*60*60))%24
+    print (hours,":",minutes,":",seconds)
 
 
 millis=input("Enter time in milliseconds: ")
